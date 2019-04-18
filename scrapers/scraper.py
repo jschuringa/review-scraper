2d27f7e272271845f3ec8d2d69e4d8836467dcb7
Joey Schuringa <joey@milyli.com>
2019-04-18 12:46:08 -0500

    feat(Scraper): Use full browser to avoid headless detection

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -8,9 +8,11 @@ class Scraper():
     @staticmethod
     def get_reviews(url):
         options = Options()
-        options.add_argument("--headless")
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        options.add_argument(f'user-agent={user_agent}')
         driver = webdriver.Chrome(chrome_options=options)
         driver.get(url)
+        driver.maximize_window()
         soup = BeautifulSoup(driver.page_source, "html5lib")
         print(soup)
         items = soup.findAll("div", {"class": "review-wrapper"})
