a90e77bfbf1fcae5586f7b53f5f8f0ba4f54db49
Joey Schuringa <jschuringa86@gmail.com>
2019-04-18 19:25:09 -0500

    feat(Scraper): Add in more app tests

--- tests/app-tests.py
+++ tests/app-tests.py
@@ -1,11 +1,15 @@
 import unittest
 from unittest.mock import MagicMock
 from app import app
-from scrapers.scraper import Scraper
+from scraper.scraper import Scraper
 
 class TestAppCases(unittest.TestCase):
 
     def test_url_is_restaurant_page_succeeds(self):
+        """
+            Ensures the endpoint succeeds when the url is a restaurant url
+        """
+        Scraper.get_scraper = MagicMock(return_value=Scraper([]))
         Scraper.get_reviews = MagicMock(return_value=[])
         with app.test_client() as client:
             sent = {"url": "https://www.grubhub.com/restaurant/hashbrowns-on-wells-1155-n-wells-st-chicago/287727"}
@@ -13,6 +17,10 @@ class TestAppCases(unittest.TestCase):
             self.assertEqual(result.status_code, 200)
 
     def test_url_is_reviews_page_succeeds(self):
+        """
+            Ensures the endpoint succeeds when the url is a restaurant review url
+        """
+        Scraper.get_scraper = MagicMock(return_value=Scraper([]))
         Scraper.get_reviews = MagicMock(return_value=[])
         with app.test_client() as client:
             sent = {"url": "https://www.grubhub.com/restaurant/hashbrowns-on-wells-1155-n-wells-st-chicago/287727/reviews"}
@@ -20,7 +28,33 @@ class TestAppCases(unittest.TestCase):
             self.assertEqual(result.status_code, 200)
 
     def test_url_is_not_restaurant_or_reviews_page_fails(self):
+        """
+            Ensures the endpoint fails when the url is a not a restaurant or review url
+        """
         with app.test_client() as client:
             sent = {"url": "www.google.com"}
             result = client.post('/scrape_reviews', data=sent)
             self.assertEqual(result.status_code, 400)
+
+    def test_url_not_in_post(self):
+        """
+            Ensures the endpoint fails when the url is not posted
+        """
+        with app.test_client() as client:
+            sent = {}
+            result = client.post('/scrape_reviews', data=sent)
+            self.assertEqual(result.status_code, 400)
+
+    def test_happy_path(self):
+        """
+            Ensures the endpoint returns a dictionary as json
+        """
+        reviewMockData = {"author": "test"}
+        Scraper.get_scraper = MagicMock(return_value=Scraper([]))
+        Scraper.get_reviews = MagicMock(return_value=[reviewMockData])
+        with app.test_client() as client:
+            sent = {"url": "https://www.grubhub.com/restaurant/hashbrowns-on-wells-1155-n-wells-st-chicago/287727"}
+            result = client.post('/scrape_reviews', data=sent)
+            print(result)
+            self.assertEqual(result.status_code, 200)
+            
