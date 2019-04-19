d2f5ec4838c3e72407c3425a2b9cb5476bd74609
Joey Schuringa <jschuringa86@gmail.com>
2019-04-18 19:38:13 -0500

    chore(Setup): Add linux and mac setup files

--- setup_mac.py
+++ setup_mac.py
@@ -4,9 +4,9 @@ import wget
 
 dirname = os.path.dirname(__file__)
 path = os.path.join(dirname, 'venv/bin')
-file_path = path + "/chromedriver_linux64.zip"
+file_path = path + "/chromedriver_mac64.zip"
 if not os.path.isfile(file_path):
-    url = 'https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip'
+    url = 'https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_mac64.zip'
     wget.download(url, out=path)
 
     zip_ref = zipfile.ZipFile(file_path, 'r')
