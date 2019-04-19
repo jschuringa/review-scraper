55e5128754c3cccbb8e6d4df2788e1b7e39fc5af
Joey Schuringa <joey@milyli.com>
2019-04-19 12:42:00 -0500

    test(Scraper): Add data structure tests

--- scraper/tests/scraper_tests.py
+++ scraper/tests/scraper_tests.py
@@ -1,8 +1,8 @@
 import unittest
 from unittest.mock import Mock, patch
 from datetime import date, timedelta
-from ..scraper import Scraper
-from ..parser import ReviewItem
+from scraper import scraper
+from scraper import parser
 from .test_constants import test_html_default, test_html_with_response, test_html_top_reviewer, test_html_invalid
 
 class TestScraperMethods(unittest.TestCase):
@@ -12,13 +12,21 @@ class TestScraperMethods(unittest.TestCase):
             Asserts a scraper with no item data does not create
             any garbage entries
         """
-        scraper = Scraper([])
-        result = scraper.get_reviews()
+        test_scraper = scraper.Scraper([])
+        result = test_scraper.get_reviews()
         self.assertEqual(len(result), 0)
     
-    @patch("ReviewItem.build_review_item")
-    def test_get_reviews(self):
-        item1 = ReviewItem("me", "this is the first review", 2, date.today(), False, ["sandwich"], True)
-        item2 = ReviewItem("you", "this is the second review", 4, date.today() - timedelta(1), True, ["chicken", "onion rings"], False)
-        scraper = Scraper([1, 2])
+    @patch("scraper.parser.ReviewItem.build_review_item")
+    def test_get_reviews(self, mock_build):
+        """
+            Asserts get_reviews builds the correct data structure
+        """
+        item1 = parser.ReviewItem("me", "this is the first review", 2, date.today(), False, ["sandwich"], True)
+        item2 = parser.ReviewItem("you", "this is the second review", 4, date.today() - timedelta(1), True, ["chicken", "onion rings"], False)
+        mock_build.side_effect = [item1, item2]
+        test_scraper = scraper.Scraper([1, 2])
+        result = test_scraper.get_reviews()
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0], item1.__dict__)
+        self.assertEqual(result[1], item2.__dict__)
 
